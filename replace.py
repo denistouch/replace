9a5104e2190adeea5d481d2cff725e31c903da99
de.s.smirnov <de.s.smirnov@vkteam.ru>
2025-08-21 11:29:30 +0300

    рефакторинг

--- replace.py
+++ replace.py
@@ -8,7 +8,7 @@ def is_file_and_starts_with(filename: str, folder_path: str, prefix: str) -> boo
     return os.path.isfile(path) and filename.startswith(prefix)
 
 
-def combine_errors(errors: list) -> dict:
+def build_errors_hash(errors: list) -> dict:
     errors_hash = {}
     for error in errors:
         file = error['file']
@@ -20,8 +20,8 @@ def combine_errors(errors: list) -> dict:
     return errors_hash
 
 
-def prepare_errors(errors: list) -> str:
-    errors_hash = combine_errors(errors)
+def build_error_msg(errors: list) -> str:
+    errors_hash = build_errors_hash(errors)
     error_report = ''
     for reason, files in errors_hash.items():
         files_list = '\n\t'.join(files)
@@ -33,22 +33,22 @@ def prepare_errors(errors: list) -> str:
     return 'В ходе выполнения возникли следующие ошибки:\n' + error_report
 
 
-def prepare_report(raw_report: dict) -> str:
+def build_report_msg(raw_report: dict) -> str:
     if raw_report['total'] == 0:
         return 'В папке не найдены файлы'
 
     if raw_report['found'] == 0:
         return f'В папке отсутствуют файлы с префиксом {raw_report["prefix"]}'
 
-    changed = f'В папке {raw_report["folder_path"]} найдено {raw_report["found"]} файлов с префиксом "{raw_report["prefix"]}", успешно переименовано {raw_report["renamed"]}.'
+    output = f'В папке {raw_report["folder_path"]} найдено {raw_report["found"]} файлов с префиксом "{raw_report["prefix"]}", успешно переименовано {raw_report["renamed"]}.'
 
     if len(raw_report['errors']) != 0:
-        changed = f'{changed}\n' + prepare_errors(raw_report['errors'])
+        output = f'{output}\n' + build_error_msg(raw_report['errors'])
 
-    return changed
+    return output
 
 
-def rename_in_folder(folder_path: str, prefix: str, replacement: str) -> str:
+def replace(folder_path: str, prefix: str, replacement: str) -> dict:
     total, found, renamed, errors = 0, 0, 0, []
 
     for filename in os.listdir(folder_path):
@@ -71,14 +71,14 @@ def rename_in_folder(folder_path: str, prefix: str, replacement: str) -> str:
                 'reason': e
             })
 
-    return prepare_report({
+    return {
         'folder_path': folder_path,
         'prefix': prefix,
         'total': total,
         'found': found,
         'renamed': renamed,
         'errors': errors
-    })
+    }
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Программа по массовому отрезанию префикса от файлов в каталоге')
@@ -89,4 +89,4 @@ if __name__=='__main__':
 
     args = parser.parse_args()
 
-     print(rename_in_folder(getattr(args, 'in'), args.prefix, args.to))
+    print(build_report_msg(replace(getattr(args, 'in'), args.prefix, args.to)))
